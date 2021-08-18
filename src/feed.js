707d955def11f6f9c6beb45af4aa76c7fc004689
Pedro Barbosa <p.brbsa@gmail.com>
2021-08-18 09:05:59 -0300

    Update feed.js

--- src/feed.js
+++ src/feed.js
@@ -61,6 +61,7 @@ export function Feed() {
                 },
             ]);
         });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [startDate, endDate, language, get]);
 
     return (
